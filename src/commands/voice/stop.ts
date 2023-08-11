71b9f7b2b9340e2113ec20cd3b5899c3e526efe5
Frazer Doyle <19980075+ToxicFrazzles@users.noreply.github.com>
2023-08-11 07:54:05 +0100

    Added first bit of command to leave the voice channel

--- src/commands/voice/stop.ts
+++ src/commands/voice/stop.ts
@@ -1,10 +1,10 @@
 import {SlashCommandBuilder, CommandInteraction} from "discord.js";
-import {joinVoiceChannel, VoiceConnectionStatus} from "@discordjs/voice";
+import {getVoiceConnection, VoiceConnectionStatus} from "@discordjs/voice";
 import {Client} from "../../lib/client";
 
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('play').setDescription('Plays the requested audio in your voice channel'),
+    data: new SlashCommandBuilder().setName('stop').setDescription('Stops playing audio and leaves the voice channel'),
     async execute(client: Client, interaction: CommandInteraction){
         await interaction.reply({content: `Not yet implemented. Don't expect too much to happen`, ephemeral: true});
 
@@ -14,26 +14,16 @@ module.exports = {
 
         const channel = await guild.channels.fetch().then((channels) => {
             for(const c of channels.values()) {
-                // if(c.)
                 if(!c?.isVoiceBased()) continue;
-                // console.log("Voice Channel:", c.name);
-                // console.log(c.members.keys())
-                // console.log(interaction.user.id)
                 if(c.members.filter(x => x.id == interaction.user.id).first()) return c;
             }
         })
         if(!channel || !channel.joinable) return;
-        console.log("Channel: ", channel.name)
 
-        const vc = joinVoiceChannel({
-            channelId: channel.id,
-            guildId: channel.guild.id,
-            adapterCreator: channel.guild.voiceAdapterCreator,
-        });
-
-        vc.on(VoiceConnectionStatus.Ready, () => {
-            console.log("Voice connection ready!");
-        });
+        const vc = getVoiceConnection(guild.id);
+        if(!vc) return;
+        console.log("Disconnecting...")
+        vc.destroy();
 
     },
 };
